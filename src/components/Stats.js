624fbe6246e9789d04624e1424944e90620bf683
alexawesomecode <alexawesomecode@gmail.com>
2020-06-16 19:40:51 +0000

    incorpored new progressbar component to stats

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -1,53 +1,14 @@
 import React from 'react';
-import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
+import ProgressBar from './ProgressBar'
 
 const Stats = () => (
 
   <div className="stats-circles">
 
-    <CircularProgressbar
-      value={Math.floor(Math.random() * 100)}
-      text={`${Math.floor(Math.random() * 100)}%`}
-      styles={buildStyles({
-        rotation: 0.25,
-        strokeLinecap: 'butt',
-        textSize: '8px',
-        pathTransitionDuration: 0.5,
-        pathColor: 'orange',
-        textColor: '#f88',
-        trailColor: '#d6d6d6',
-        backgroundColor: '#3e98c7',
-      })}
-    />
-    <CircularProgressbar
-      value={Math.floor(Math.random() * 100)}
-      text={`${Math.floor(Math.random() * 100)}%`}
-      styles={buildStyles({
-        rotation: 0.25,
-        strokeLinecap: 'butt',
-        textSize: '8px',
-        pathTransitionDuration: 0.5,
-        pathColor: 'blue',
-        textColor: '#f88',
-        trailColor: '#d6d6d6',
-        backgroundColor: '#3e98c7',
-      })}
-    />
-    <CircularProgressbar
-      value={Math.floor(Math.random() * 100)}
-      text={`${Math.floor(Math.random() * 100)}%`}
-      styles={buildStyles({
-        rotation: 0.25,
-        strokeLinecap: 'butt',
-        textSize: '8px',
-        pathTransitionDuration: 0.5,
-        pathColor: 'deeppink',
-        textColor: '#f88',
-        trailColor: '#d6d6d6',
-        backgroundColor: '#3e98c7',
-      })}
-    />
-    ;
+    <ProgressBar value={92} path='orange' text='#f88' trail='#d6d6d6' />
+    <ProgressBar value={12} path='blue' text='#f88' trail='#d6d6d6' />
+    <ProgressBar value={48} path='deeppink' text='#f88' trail='#d6d6d6' />
+
   </div>
 );
 
