3a1a0e63acac2f1860a485207e2c212d101a3381
alexawesomecode <alexawesomecode@gmail.com>
2020-06-23 18:54:24 +0000

    worked on eslint: absolute import fix #5

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import './index.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+import './index.css';
 import App from './components/App';
 import reducer from './reducers/index';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 const initialState = [
   {
     name: 'biceps',
