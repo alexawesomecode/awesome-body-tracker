9dab05a876bfbcb0dd893b0f5418b1e10776cf00
alexawesomecode <alexawesomecode@gmail.com>
2020-06-23 18:52:45 +0000

    worked on eslint: absolute import fix #4

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import './index.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './components/App';
 import reducer from './reducers/index';
 
